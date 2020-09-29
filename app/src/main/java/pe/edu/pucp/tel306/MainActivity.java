f949e16a988690ead637a8800aba40a7a6a30980
AlonsoGuevara15 <a20160679@pucp.edu.pe>
2020-09-29 08:23:37 -0500

    first commit

---
+++ app/src/main/java/pe/edu/pucp/tel306/MainActivity.java
@@ -0,0 +1,14 @@
+package pe.edu.pucp.tel306;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
